a1af76c1e69333eae3f30a002a0ae077edb8bcf3
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-01-17 10:58:51 -0300

    Create index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const NavItems = [
+    {
+        label: 'Home',
+        href: '/'
+    },
+    {
+        label: 'Portfólio',
+        href: '/portfolio'
+    },
+    {
+        label: 'Quem somos?',
+        href: '/about'
+    },
+    {
+        label: 'Serviços',
+        href: '/servicos'
+    },
+    {
+        label: 'Contato',
+        href: '/contato'
+    },
+];
