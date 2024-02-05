d058f172ba70d57d45eccdbc4cb6dcbe96c15be8
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-02-05 13:05:55 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -20,3 +20,66 @@ export const NavItems = [
         href: '/contato'
     },
 ];
+export const FooterQuickLinks = [
+    {
+        label: 'Home',
+        href: '/'
+    },
+    {
+        label: 'Biography',
+        href: '/biography'
+    },
+    {
+        label: 'Investor Acumen',
+        href: '/investoracumen'
+    },
+    {
+        label: 'Publications',
+        href: '/publications'
+    },
+    {
+        label: 'Social Media',
+        href: '/socialmedia'
+    },
+    {
+        label: 'Personal Blog',
+        href: '/personalblog'
+    },
+]
+export const FooterResources = [
+    {
+        label: "Faq's",
+        href: '/faqs'
+    },
+    {
+        label: "Lorem Ipsum",
+        href: '/faqs'
+    },
+    {
+        label: "Lorem Ipsum",
+        href: '/faqs'
+    },
+]
+
+export const FooterContact = [
+    {
+        label: 'Facebook',
+        href: '/personalblog'
+    },
+    {
+        label: 'Linkedin',
+        href: '/personalblog'
+    },
+    {
+        label: 'Twitter',
+        href: '/personalblog'
+    },
+    {
+        label: 'Seeking Alpha',
+        href: '/personalblog'
+    },
+    {
+        label: 'Investor Acumen',
+        href: '/personalblog'
+    },
+]
