b6d670808a507dc74550fc80a37c735c99efab99
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-02-05 13:05:45 -0300

    Update Navbar.tsx

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,40 +3,54 @@
 import React, { useState } from 'react'
 import { MenuIcon, X } from 'lucide-react'
 
-import Container from '../Container'
 import NavLinks from './NavLinks'
 
 import Link from 'next/link'
 import Image from 'next/image'
 
 import logo from '../../../../public/Logo.svg'
+import NavLinksMobile from './NavLinksMobile'
+import Container from '../Container'
 
 function Navbar() {
     const [open, setOpen] = useState<boolean>(false)
 
     return (
-        <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
-            <header className='relative bg-white border-b h-16'>
-                <Container className='flex h-16'>
-                    <div className='flex justify-between items-center w-full lg:w-auto'>
-                        <div className='ml-4 md:ml-2 self-stretch flex items-center'>
-                            <Link href={'/'}>
-                                <Image
-                                    src={logo}
-                                    alt='header logo'
-                                />
-                            </Link>
+        <>
+
+            <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+                <header className='relative bg-white border-b h-16'>
+                    <Container className='flex h-16'>
+                        <div className='flex justify-around items-center w-full'>
+                            <div className='ml-4 md:ml-2 self-stretch flex items-center'>
+                                <Link href={'/'}>
+                                    <Image
+                                        src={logo}
+                                        alt='header logo'
+                                    />
+                                </Link>
+                            </div>
+                            <div className='lg:hidden duration-300 hover:cursor-pointer' onClick={() => setOpen(!open)}>
+                                {open ? <X /> : <MenuIcon />}
+                            </div>
+                            <div className='w-auto hidden lg:flex gap-12 justify-center items-center h-full'>
+                                <NavLinks />
+                            </div>
                         </div>
-                        <div className='lg:hidden duration-300 hover:cursor-pointer' onClick={() => setOpen(!open)}>
-                            {open ? <MenuIcon /> : <X />}
-                        </div>
-                        <div className='w-auto hidden lg:flex gap-12 justify-center items-center'>
-                            <NavLinks />
+                    </Container>
+                </header>
+            </div>
+            {/*Mobile Nav*/}
+            <div className={`lg: z-10 pt-20 bg-white fixed border-r w-72 h-full duration-500 ${open ? 'left-0' : 'left-[-100%]'}`}>
+                <div className='h-screen w-full'>
+                    <div className='flex flex-col w-full justify-start gap-8'>
+                        <div className='flex w-full overflow-hidden flex-col mt-2 gap-8'>
+                            <NavLinksMobile />
                         </div>
                     </div>
-                </Container>
-            </header>
-        </div>
+                </div>
+            </div>
+        </>
     )
 }
 
