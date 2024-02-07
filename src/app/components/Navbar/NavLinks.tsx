38f51a2ee1df92cb67a90ee6362008147c94e481
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-02-07 12:33:55 -0300

    Update NavLinks.tsx

--- src/app/components/Navbar/NavLinks.tsx
+++ src/app/components/Navbar/NavLinks.tsx
@@ -8,7 +8,7 @@ function NavLinks() {
       {NavItems.map((item, index) => {
         return (
           <Link key={index} href={item.href}>
-            <div className='font-rhd text-sm'>
+            <div className='font-rhd text-sm hover:bg-sky-400 rounded-sm px-3 py-2 hover:text-white transition duration-300 ease-in-out'>
               {item.label}
             </div>
           </Link>
