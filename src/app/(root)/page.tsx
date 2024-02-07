e6af2bc5bca55e3877cac4c0a47ffc11ee8de3e4
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-02-07 18:45:21 -0300

    Update page.tsx

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,19 @@ import Link from "next/link";
 import Container from "../components/Container";
 
 import Image from "next/image";
-import HomeImage from '../../../public/3imagehome.svg'
+import HomeImage from '../../../public/3imageshome.png'
+import HomeCard from "../components/HomeCard";
+import HomeDesign from '../../../public/designHome.png'
+import DesignIconHome from '../../../public/DesignIconHome.svg'
+import WebHome from '../../../public/WebHome.png'
+import WebIconHome from '../../../public/WebIconHome.svg'
+import ServerIconHome from '../../../public/ServerIconHome.svg'
+import ServerHome from '../../../public/ServerHome.png'
+import MobileHome from '../../../public/MobileHome.png'
+import AndroidIconHome from '../../../public/AndroidIconHome.svg'
+import IOSIconHome from '../../../public/IOSIconHome.svg'
+import IAIconHome from '../../../public/IAIconHome.svg'
+import IAHome from '../../../public/IAHome.png'
 
 export default function Home() {
   return (
@@ -17,9 +29,9 @@ export default function Home() {
                 <span className="text-sky-400"> escalar</span> e <span className="text-sky-500">impulsionar</span>.
               </h1>
               <Link href={'/'}>
-                <div className="w-[15rem] flex items-center justify-center h-8 bg-sky-500 hover:bg-sky-700 duration-300 rounded-full">
+                <button className="w-[15rem] flex items-center justify-center h-8 bg-sky-500 hover:bg-sky-700 duration-300 rounded-full">
                   <span className="text-white font-rhd font-semibold tracking-wide text-sm">PEÇA JÁ SEU ORÇAMENTO!</span>
-                </div>
+                </button>
               </Link>
             </div>
             <div>
@@ -36,6 +48,81 @@ export default function Home() {
           Desenvolvendo sistemas que fazem mais do que <span className="text-teal-300">resolver problemas</span>; eles abrem novas <span className="text-cyan-300">oportunidades</span>.
         </h1>
       </div>
+      <section className="h-[420rem] xlg:h-[230rem] bg-homebg bg-cover bg-no-repeat bg-center">
+        <Container>
+          <div className="flex flex-col xlg:flex-row items-center justify-center gap-40 pl-6 pt-20 lg:pt-32">
+            <HomeCard
+              icon={DesignIconHome}
+              title="UI/X Design"
+              content="Transformamos ideias em realidade visual com designs de alta qualidade e centrados no usuário. 
+                Nosso objetivo é criar uma experiência visual envolvente que não apenas atraia, mas também converta visitantes em clientes fiéis."
+              className="h-[32rem]"
+            />
+            <Image
+              src={HomeDesign}
+              alt="home design image"
+            />
+          </div>
+          <div className="flex flex-col xlg:flex-row items-center justify-center gap-40 pl-6 pt-20 lg:pt-32">
+            <Image
+              src={WebHome}
+              alt="home web image"
+            />
+            <HomeCard
+              icon={WebIconHome}
+              title="Páginas Web Interativas"
+              content="Transformamos ideias em realidade visual com designs de alta qualidade e centrados no usuário. 
+                Nosso objetivo é criar uma experiência visual envolvente que não apenas atraia, mas também converta visitantes em clientes fiéis."
+              className="h-[35rem]"
+            />
+          </div>
+          <div className="flex flex-col xlg:flex-row items-center justify-center gap-40 pl-6 pt-20 lg:pt-32">
+            <HomeCard
+              icon={ServerIconHome}
+              title="Sistemas Web e Desktop"
+              content="Desenvolvemos sistemas robustos e escaláveis para web e desktop,
+               personalizados para atender às necessidades específicas do seu negócio. Com foco em segurança e eficiência,
+                nossas soluções são projetadas para otimizar seus processos operacionais."
+              className="h-[36rem]"
+            />
+            <Image
+              src={ServerHome}
+              alt="home web image"
+            />
+          </div>
+          <div className="flex flex-col xlg:flex-row items-center justify-center gap-40 pl-6 pt-20 lg:pt-32">
+            <Image
+              src={MobileHome}
+              alt="home web image"
+              layout="fixed"
+            />
+            <HomeCard
+              icon={AndroidIconHome}
+              icon2={IOSIconHome}
+              title="Aplicativos iOS e Android"
+              content="Expandimos o alcance do seu negócio com aplicativos móveis intuitivos e de alto desempenho para plataformas iOS e Android.
+               Nossos aplicativos são projetados para oferecer uma experiência de usuário impecável,
+               aumentando a retenção e o engajamento do cliente."
+              className="h-[40.7rem]"
+            />
+          </div>
+          <div className="flex flex-col xlg:flex-row items-center justify-center gap-40 pl-6 pt-20 lg:pt-32">
+            <HomeCard
+              icon={IAIconHome}
+              title="Sistemas Web e Desktop"
+              content="Desenvolvemos sistemas robustos e escaláveis para web e desktop,
+               personalizados para atender às necessidades específicas do seu negócio. Com foco em segurança e eficiência,
+                nossas soluções são projetadas para otimizar seus processos operacionais."
+              className="h-[38.5rem]"
+            />
+            <Image
+              src={IAHome}
+              alt="home web image"
+              layout="fixed"
+            />
+          </div>
+        </Container>
+      </section>
     </>
 
   )
