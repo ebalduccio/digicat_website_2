cb8593760568893cf7d1850818c51ddf1fb60a0e
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-02-08 00:50:51 -0300

    Update HomeCard.tsx

--- src/app/components/HomeCard.tsx
+++ src/app/components/HomeCard.tsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import { cn } from '@/lib/utils';
+
 import Image from 'next/image'
+
 import Link from 'next/link';
 
-import React from 'react'
 
 type CardProps = {
     title: string;
