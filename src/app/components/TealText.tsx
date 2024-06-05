7773150d56fa247bf668bbea3999e5629ee1e94e
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-06-05 17:36:16 -0300

    Create TealText.tsx

---
+++ src/app/components/TealText.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TealText = ({ content }: TextProps) => {
+    return (
+        <span className="text-teal-300">{content}</span>
+    )
+}
+
+export default TealText
