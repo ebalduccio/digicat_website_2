a359a6311305e736ca379834c4ba09bf6dab0840
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-06-05 16:38:58 -0300

    Update Card.tsx

--- src/app/about/components/Card.tsx
+++ src/app/about/components/Card.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 
-const Card = ({title, content} : AboutProps) => {
+const Card = ({title, content} : CardAboutProps) => {
   return (
-    <div className='w-72 h-72 rounded-md shadow-md bg-white group hover:bg-homeblue'>
+    <div className='w-72 h-72 rounded-md shadow-md bg-white group hover:bg-homeblue duration-200'>
         <div className='h-16 text-center pt-4'>
             <h1 className='text-2xl group'>{title}</h1>
         </div>
-        <div className='h-3 bg-homeblue group-hover:bg-white'></div>
+        <div className='h-3 bg-homeblue group-hover:bg-white duration-200'></div>
         <div className='text-center pt-20 flex items-center justify-center'>
             <span className='max-w-xs text-xl'>{content}</span>
         </div>
