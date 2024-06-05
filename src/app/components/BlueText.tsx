4aa001d05101ab05b7aab5be819c4442ff17c887
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-06-05 17:36:18 -0300

    Create BlueText.tsx

---
+++ src/app/components/BlueText.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlueText = ({ content }: TextProps) => {
+    return (
+        <span className="text-homeblue">{content}</span>
+    )
+}
+
+export default BlueText
