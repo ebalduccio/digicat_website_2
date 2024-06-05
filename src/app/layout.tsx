6c2674b53051f1a16b7423145cb46e8c9343c1d6
ebalduccio <140448069+ebalduccio@users.noreply.github.com>
2024-06-05 18:53:47 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next'
-import './styles/globals.css'
-import { cn } from '@/lib/utils'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import type { Metadata } from 'next';
+import './styles/globals.css';
+import { cn } from '@/lib/utils';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import { NavbarProvider } from './context/NavContext';
 
 export const metadata: Metadata = {
   title: 'Digicat',
@@ -15,21 +16,21 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className='h-full '>
       <body className={cn('relative h-full antialiased font-rhd')}>
-        <main className='absolute sm:relative flex flex-col min-h-screen'>
-          <div className='flex-grow flex-1'>
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </main>
+        <NavbarProvider>
+          <main className='absolute sm:relative flex flex-col min-h-screen'>
+            <div className='flex-grow flex-1'>
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </main>
+        </NavbarProvider>
       </body>
     </html>
-  )
+  );
 }
-
-
